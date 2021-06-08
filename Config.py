a98f50f8ff9875bdbc3c8ffb312b71b73b2c9564
Kgf123 <rutwiks123@gmail.com>
2021-06-08 15:01:23 +0530

    Initial commit

---
+++ Config.py
@@ -0,0 +1,17 @@
+# # -*- coding: utf-8 -*-
+
+# @author: Kgf123
+# Licensed Under 'GPU v3.0'
+# Copyright (C) 2021 https://github.com/Kgf123
+# This file is part of Project-Autowaifu.
+# Pproject-Autowaifu must not be copied and/or distributed without the express permission of Kgf123.
+# All rights resrved
+
+import os
+
+
+# Get these values from my.telegram.org
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+# Get this from https://replit.com/@TeamUltroid/UltroidStringSession
+STRING_SESSION = os.environ.get("STRING_SESSION")
